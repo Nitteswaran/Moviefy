cb6af6b03d36216ed1f95ee06e724eb47d959e0e
Nitteswaran <spnittes@gmail.com>
2025-09-22 13:05:37 +0800

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = "1c4f84983493828b5f40621e97949913";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results;
+};
+
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results;
+};
