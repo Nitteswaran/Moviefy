54fa5e6ff58aa7179006aea0a54b931626ce235c
Nittes <30345503+Nitteswaran@users.noreply.github.com>
2025-09-22 18:30:59 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "1c4f84983493828b5f40621e97949913";
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
